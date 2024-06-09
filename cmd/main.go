fb0ac1980e1e682ed2c1267d2b36707929c46b2b
Шанталь <shantal.zhansugurova@mail.ru>
2024-06-09 15:20:23 +0200

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,10 @@
-package cmd
+package main
 
 import (
 	"Caesar/logic/coder"
 	"flag"
 	"fmt"
+	"strings"
 )
 
 // Цезарь это структура, содержит внутри алфавит, для использования де/энкрипта
@@ -11,15 +12,42 @@ import (
 
 func main() {
 	const EnglishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	var text = flag.String("encrypt", "-", "Target value")
+	var text = flag.String("encrypt", "", "Target value")
 	var shifts = flag.Uint("shift", 0, "Target value")
-	var text1 = flag.String("decrypt", "-", "Target value")
-	//var shifts1 = flag.Uint("shift", 0, "Target value")
+	var text1 = flag.String("decrypt", "", "Target value")
+
 	caesar := coder.NewCaesar(EnglishAlphabet)
 	flag.Parse()
 	fmt.Println("original text:", *text)
 	fmt.Println("shift :", *shifts)
 	fmt.Println("original text:", *text1)
-	fmt.Println(caesar.Encrypt(*text, *shifts))
-	fmt.Println(caesar.Decrypt(*text1, *shifts))
+	if valid(*text1, EnglishAlphabet) == false {
+		fmt.Println("ошибка ")
+	} else {
+		fmt.Println(caesar.Encrypt(*text, *shifts))
+		fmt.Println(caesar.Decrypt(*text1, *shifts))
+	}
+	if valid(*text, EnglishAlphabet) == false {
+		fmt.Println("ошибка ")
+	} else {
+		fmt.Println(caesar.Encrypt(*text, *shifts))
+		fmt.Println(caesar.Decrypt(*text1, *shifts))
+	}
+	//fmt.Println(caesar.Encrypt(*text, *shifts))
+	//fmt.Println(caesar.Decrypt(*text1, *shifts))
+}
+func valid(data string, alphabet string) bool {
+	dataList := strings.Split(data, "")
+	alphabetList := strings.Split(alphabet, "")
+	for _, value1 := range dataList {
+		for _, value2 := range alphabetList {
+			//log.Printf("value:%v, value2:%v\n", value1, value2)
+			if value1 == value2 {
+
+				return true
+			}
+		}
+
+	}
+	return false
 }
